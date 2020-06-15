9d035b2b6abe5ed1a29f6a272414aa1257f32910
Declan Lubbersen <dtslubbersen@gmail.com>
2020-06-15 21:59:57 +0200

    Added necessary files.

---
+++ config.js
@@ -0,0 +1,33 @@
+const config = module.exports = {};
+
+// ADD THESE TO THAW BOT
+config.emails = [
+	'okg46545@eoopy.com',
+	'ioi78972@zzrgg.com',
+	'jum28037@eoopy.com',
+	'elu72397@eoopy.com',
+	'xmn32656@bcaoo.com',
+	'rxg13090@zzrgg.com',
+	'htt57256@eoopy.com',
+	'ofb81179@eoopy.com',
+	'bsy76356@eoopy.com',
+	'dft66311@eoopy.com',
+	'xrh90093@zzrgg.com',
+	'fao61366@zzrgg.com',
+	'yrz62616@zzrgg.com',
+	'qwr85021@eoopy.com',
+	'zvg20353@bcaoo.com',
+	'kkj92492@zzrgg.com',
+	'jlw95033@eoopy.com',
+	'nvh54755@zzrgg.com',
+	'mbv26648@eoopy.com',
+	'wom79625@zzrgg.com',
+];
+
+config.emailsTwo = [
+	'zdg61264@zzrgg.com',
+];
+
+config.personalEmails = [
+	'warzonediscordjsbot@gmail.com',
+];
